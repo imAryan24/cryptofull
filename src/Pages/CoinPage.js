57820446dca7dbae8176d4da9c8b2545b862ecac
imAryan24 <95496583+imAryan24@users.noreply.github.com>
2022-07-01 00:21:13 +0530

    Update CoinPage.js

--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -1,6 +1,29 @@
 import React from 'react'
-
+import { SingleCoin } from '../config/api';
+import { useParams } from 'react-router-dom'
+import { useState } from 'react';
+import { CryptoState } from '../Context';
+import axios from 'axios';
+import { useEffect } from 'react';
 const CoinPage = () => {
+
+  const { id } = useParams();
+  const [coin, setCoin] = useState();
+
+  const { currency, symbol } = CryptoState();
+
+  const fetchCoin = async () => {
+    const { data } = await axios.get(SingleCoin(id));
+
+    setCoin(data);
+  };
+  console.log(coin);
+
+  
+  useEffect(()=> {
+  fetchCoin();
+   // eslint-disable-next-line react-hooks/exhaustive-deps
+}, []);
   return (
     <div>CoinPage</div>
   )
